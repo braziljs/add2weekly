cdabe66768d1a44b57da4c1d892acc91925b53a7
Keppelen <keppelen.as@gmail.com>
2012-12-12 20:08:30 -0200

    new layout

--- add2weekly/bookmarklet.js
+++ add2weekly/bookmarklet.js
@@ -1,5 +1,5 @@
 (function(d){
-    
+
     var iSrc= 'http://braziljs.org/services/add2weekly/mdl-add2weekly.php?',
     create= function(t, l, c, dv){
         var i= document.createElement('iframe');
@@ -11,21 +11,20 @@
         dv.style.left= '0px';
         dv.style.zIndex= '99999999';
         dv.style.margin= '0px';
-        dv.style.width= '300px';
-        dv.style.height= '280px';
-        dv.style.border= 'solid 1px black';
+        dv.style.width= '550px';
+        dv.style.height= '450px';
         dv.style.boxShadow= '0px 0px 30px black';
         dv.id= 'brazilJS-add2wkly';
         i.style.border= 'none';
         i.style.height= '100%';
         i.style.width= '100%';
-        dv.innerHTML= "<input id=braziljsa2w-closeBtn type=button value=Close style='position:absolute;left:154px;bottom:29px;' />";
+        dv.innerHTML= "<input id=braziljsa2w-closeBtn type=button value=Close style='' />";
         dv.appendChild(i);
-        
+
         d.getElementById('braziljsa2w-closeBtn').onclick= function(){
             dv.parentNode.removeChild(dv);
         };
-        
+
         /*i.onload= function(){
             i.contentWindow.ref= d;
         };*/
